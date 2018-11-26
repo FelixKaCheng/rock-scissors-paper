ae6afff34fc6360aed495733702691bb42155459
Student <jsitbon@qcc.cuny.edu>
2018-11-26 17:56:56 -0500

    Build out App.js and added PlayerCard.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import PlayerCard from "./PlayerCard";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-Everyone loves puppies
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+class Game extends Component{
+constructor(){
+    super();
+    this.signs=["rock","scissors","paper"]; // Where does this come from?
+    this.state={
+      playerOne:"rock",
+      playerTwo: "scissors"
+    }
   }
+
+// We put our methods here...
+playGame=()=>{
+  this.setState({
+    playerOne:this.signs[Math.floor(Math.random()*3)],
+    playerTwo:this.signs[Math.floor(Math.random()*3)]
+  })
+}
+render(){
+  return(
+    <div className="style">
+      <div>
+      <PlayerCard sign={this.state.playerOne}/>
+      <PlayerCard sign={this.state.playerTwo}/>
+
+      <PlayerCard/>
+      </div>
+    <div className="winner">Winner</div>
+    <button type="button" onClick={this.playGame}>Play the Game</button>
+    </div>
+
+  )
+}
 }
 
-export default App;
+export default Game;
