490e8d8fe7d227455d6029830cebadf3e58a261d
Student <jsitbon@qcc.cuny.edu>
2018-11-26 16:27:39 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+Everyone loves puppies
           </p>
           <a
             className="App-link"
